34bd7d68d25557735c6bc16eca3e2415388cc3e4
Eduardo Camaratta <eduardo.camaratta@cariad.technology>
2023-09-19 20:23:01 +0200

    Format the date with milliseconds

--- Sources/LockStateObserver/Extensions/String.swift
+++ Sources/LockStateObserver/Extensions/String.swift
@@ -4,7 +4,7 @@ extension String {
     func formatWithDate(date: Date?) -> String {
         guard let date else { return self }
         let dateFormatter = DateFormatter()
-        dateFormatter.timeStyle = .long
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss.SSS"
         return "\(dateFormatter.string(from: date)): \(self)\n"
     }
 }
