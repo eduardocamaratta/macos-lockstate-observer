8d77e8284f9f23b663d37e937ff122bbafbd2495
Eduardo Camaratta <eduardo.camaratta@cariad.technology>
2024-03-26 13:51:43 +0100

    Remove unused function

--- Sources/LockStateObserver/Extensions/String.swift
+++ Sources/LockStateObserver/Extensions/String.swift
@@ -7,8 +7,4 @@ extension String {
         dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss.SSS"
         return "\(dateFormatter.string(from: date)): \(self)\n"
     }
-
-    var prefixSpaceIfNotEmpty: String {
-        isEmpty ? "" : " \(self)"
-    }
 }
