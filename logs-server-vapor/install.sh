10fdceafd824305fd099b337148b82afb296f549
Eduardo Camaratta <eduardo.camaratta@cariad.technology>
2026-01-21 14:39:12 +0100

    Copy installation script to the vapor server (#21)
    Fixes #20

---
+++ logs-server-vapor/install.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env zsh
+
+# Build
+swift build -c release;
+
+# Copy the binary
+BIN_PATH=$(swift build -c release --show-bin-path);
+BIN_NAME=$(swift package dump-package  | sed -n '/targets/,/name/p' | grep name | sed -E 's/^.*"name"\ :\ "([^"]+)",?/\1/');
+BIN_TARGET_PATH=$HOME/Library/EduardoCamaratta;
+mkdir -p $BIN_TARGET_PATH;
+BIN_TARGET_FULL_PATH="$BIN_TARGET_PATH/$BIN_NAME";
+cp $BIN_PATH/$BIN_NAME $BIN_TARGET_FULL_PATH;
+
+# Edit the plist and copy it to the LaunchAgents
+PLIST_NAME=com.eduardocamaratta.lockstatelogsserver.plist;
+PLIST_FULL_PATH=plists/$PLIST_NAME;
+PLIST_FULL_TARGET_PATH=$HOME/Library/LaunchAgents/$PLIST_NAME;
+sed "s@\$BINARY_ABSOLUTE_PATH@$BIN_TARGET_FULL_PATH@" $PLIST_FULL_PATH > $PLIST_FULL_TARGET_PATH;
+
+# Start the daemon
+launchctl load $PLIST_FULL_TARGET_PATH;
